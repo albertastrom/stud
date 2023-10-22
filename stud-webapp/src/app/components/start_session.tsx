c94cf38d1cbb9514c7cb2f4fe3c2568d4b1de05d
Jude Partovi <jpartovi@college.harvard.edu>
2023-10-22 05:48:48 -0400

    goal table in start modal

--- stud-webapp/src/app/components/start_session.tsx
+++ stud-webapp/src/app/components/start_session.tsx
@@ -1,6 +1,69 @@
 import React, { useState } from 'react'
 import Modal from 'react-modal'
 
+function GoalTable() {
+    const [inputs, setInputs] = useState([{ text: '', isChecked: false }]);
+
+    const handleInputChange = (index: number, text: string) => {
+      const newInputs = [...inputs];
+      newInputs[index] = { text, isChecked: inputs[index].isChecked};
+      setInputs(newInputs);
+  
+      if (index === inputs.length - 1 && text.trim() !== '') {
+        setInputs([...newInputs, { text: '', isChecked: false }]);
+      } else if (text.trim() === '' && inputs.length > 1) {
+        newInputs.splice(index, 1);
+        setInputs(newInputs);
+      }
+    };
+  
+    const handleCheckboxChange = (index: number) => {
+      const newInputs = [...inputs];
+      newInputs[index].isChecked = !newInputs[index].isChecked;
+      setInputs(newInputs);
+    };
+  
+    return (
+        <div>
+            <h2 className="text-center font-semibold text-2xl text-gray-600 mt-8 grid place-content-center">
+                Add Goals
+            </h2>
+            <p className="grid place-content-center mb-4">
+                Input what you plan to do in this session to track your progress
+            </p>
+            <div className="overflow-y-auto h-32">
+                <p className="text-right mr-20">Optional?</p>
+                <table className="grid place-content-center">
+                    <tbody>
+                    {inputs.map((input, index) => (
+                        <tr key={index}>
+                        <td>
+                            <input
+                            type="text"
+                            value={input.text}
+                            onChange={(e) => handleInputChange(index, e.target.value)}
+                            className="block w-full rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6" placeholder="Add goal"
+                            />
+                        </td>
+                        <td>
+                            <input
+                            type="checkbox"
+                            checked={input.isChecked}
+                            onChange={() => handleCheckboxChange(index)}
+                            className="ml-2"
+                            />
+                        </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+
+
 const StartSessionModal = () => {
    const [isOpen, setIsOpen] = useState(false)
    const customStyles = {
@@ -26,19 +89,8 @@ const StartSessionModal = () => {
          <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} style={customStyles}>
             <div className="space-y-4">
                 <h1 className="text-5xl md:text-6xl font-bold leading-tighter tracking-tighter mb-4 grid place-content-center mt-8" data-aos="zoom-y-out">Start Session</h1>
-                
-                <div className="place-content-center">
-                    <h2 className="text-center font-semibold text-2xl text-gray-600 mt-8 grid place-content-center">
-                        Add Goals
-                    </h2>
-                    <p className="grid place-content-center">
-                        Input what you plan to do in this session to track your progress
-                    </p>
-                    <div className="w-1/3 grid place-content-center">
-                        <form>
-                            <input type="text" name="goal" id="goal" className="block w-full rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6" placeholder="Add goal"/>
-                        </form>
-                    </div>
+                <div className="grid place-content-center">
+                    <GoalTable />
                 </div>
                 <div className="py-8 grid place-content-center">
                     <button id="startSessionButton" value="start" className="p-2 cursor-pointer text-white text-3xl bg-blue-600 w-52 h-24 hover:bg-blue-700 mb-4 rounded-3xl">Start</button>
@@ -52,28 +104,3 @@ const StartSessionModal = () => {
    )
 }
 export default StartSessionModal
-
-
-// ignore
-function GoalTable() {
-    <table id="table">
-        <tr>
-            <GoalInput />
-        </tr>
-    </table>
-    const updateTable = () => {
-        var table = document.getElementById("table");
-        // Create an empty <tr> element and add it to the 1st position of the table:
-        if (table != null) {
-            //var row = table.insertRow(0);
-            //row = <GoalInput />;
-        }
-        
-    }
-    function GoalInput() {
-        return <input type="text" name="goal" id="goal" onChange={updateTable} className="block w-full rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6" placeholder="Add goal"/>
-    }
-    return <div className="grid place-content-center mt-4 overflow-y-auto h-32">
-    
-</div>
-}
