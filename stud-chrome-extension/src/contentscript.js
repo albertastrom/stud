b0644c94902fe026f5eb318400b849b79c864f19
Albert Astrom <83228526+albertastrom@users.noreply.github.com>
2023-10-22 06:03:00 -0400

    most of the extension tracking working

--- stud-chrome-extension/src/contentscript.js
+++ stud-chrome-extension/src/contentscript.js
@@ -52,43 +52,8 @@ const init = function()
     const title = document.createElement('div');
     title.className = 'timer';
 
-    // Fetching the time from storage and updating the timer accordingly
-    const fetchAndUpdateTimer = () => {
-        chrome.storage.local.get(['hours', 'minutes', 'seconds'], function(data) {
-            if (data.hours === undefined) {
-                // If it's the first time, set timer to 1 hour
-                chrome.storage.local.set({hours: 1, minutes: 0, seconds: 0});
-                title.innerText = "01:00:00";
-            } else {
-                // If not the first time, set the timer based on storage values
-                const formattedTime = `${data.hours.toString().padStart(2, '0')}:${data.minutes.toString().padStart(2, '0')}:${data.seconds.toString().padStart(2, '0')}`;
-                title.innerText = formattedTime;
-            }
-        });
-    }
+    
 
-    setInterval(() => {
-        chrome.storage.local.get(['hours', 'minutes', 'seconds'], function(data) {
-            data.seconds++;
-            if (data.seconds >= 60) {
-                data.seconds = 0;
-                data.minutes++;
-                if (data.minutes >= 60) {
-                    data.minutes = 0;
-                    data.hours++;
-                }
-            }
-
-            // Update storage with the new time values
-            chrome.storage.local.set({hours: data.hours, minutes: data.minutes, seconds: data.seconds});
-
-            const formattedTime = `${data.hours.toString().padStart(2, '0')}:${data.minutes.toString().padStart(2, '0')}:${data.seconds.toString().padStart(2, '0')}`;
-            title.innerText = formattedTime;
-        });
-    }, 1000);
-
-    // Call this function when initializing your extension's content script in each tab
-    fetchAndUpdateTimer();
 
 
     content.appendChild(title);
@@ -107,5 +72,5 @@ const init = function()
     });
 
 };
-init();
+// init(); enables the slidebox
 
