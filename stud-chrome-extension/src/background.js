d3ec3a07dace8f587d9e38ca46c1af81ef2f9d34
Hector Astrom <hector.astrom@gmail.com>
2023-10-21 22:06:59 -0400

    defined background logic for tab tracking

--- stud-chrome-extension/src/background.js
+++ stud-chrome-extension/src/background.js
@@ -1,1 +1,47 @@
-// This file is intentionally left blank.
+let activeTabId = null;
+let tabDetails = {};
+
+// Stores the active tab's time in the tabDetails list 
+// when active tab changes
+chrome.tabs.onActivated.addListener(activeInfo => {
+    activeTabId = activeInfo.tabId;
+    let currentTab = chrome.tabs.get(activeTabId)
+    // Instance a new tab in tabDetails list if it doesn't exist
+    if (!tabDetails[activeTabId]) {
+        tabDetails[activeTabId] = { time: 0, url: currentTab.pendingUrl, title: currentTab.title };
+    }
+});
+
+// set activeTabId for each window
+chrome.windows.onFocusChanged.addListener(windowId => {
+    // If no window is selected, set activeTabId to null
+    if (windowId === chrome.windows.WINDOW_ID_NONE) {
+        activeTabId = null;
+    // For all active tabs in current window (just 1 tab), take the
+    // first tab from that list and assign it to activeTabId
+    } else {
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        activeTabId = tabs[0].id;
+        let currentTab = chrome.tabs.get(activeTabId)
+        if (!tabDetails[activeTabId]) {
+            tabDetails[activeTabId] = { time: 0, url: currentTab.pendingUrl, title: currentTab.title };
+        }
+        });
+    }
+});
+
+// Built in function to increments the active tab's time
+// in the tabDetails list every second
+setInterval(() => {
+    if (activeTabId) {
+        tabDetails[activeTabId].time++;
+    }
+}, 1000);
+
+// Define function to be used in our content.js file to send the tabDetails list
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.cmd === 'getTabDetails') {
+        sendResponse(tabDetails);
+    }
+});
+
