5a9811726cc1d95c43d633ad4be7dc2a8d6b673e
Hector Astrom <hector.astrom@gmail.com>
2023-10-22 03:10:42 -0400

    added navbar and break functionality

--- stud-webapp/src/app/session/page.tsx
+++ stud-webapp/src/app/session/page.tsx
@@ -1,15 +1,36 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import { useRouter } from "next/navigation";
+import { User } from "firebase/auth";
+import { auth } from "../firebase";
 
 export default function SessionPage() {
-  const [secondsLeft, setSecondsLeft] = useState<number>(.1 * 60);
+  const [secondsLeft, setSecondsLeft] = useState<number>(15);
   const [buttonsVisible, setButtonsVisible] = useState(false);
+  const [timerColor, setTimerColor] = useState<string>("text-blue-500");
   const [buttonAnimation, setButtonAnimation] = useState("");
+  const [user, setUser] = useState<User | undefined>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const router = useRouter();
 
   useEffect(() => {
+    const checkAuthentication = async () => {
+      await auth.onAuthStateChanged(function (check_user) {
+        if (check_user) {
+          setUser(check_user);
+          setLoading(false);
+        } else {
+          router.push("/");
+        }
+      });
+    };
+    checkAuthentication();
+
     if (secondsLeft <= 0) {
       setButtonsVisible(true);
+      playSound("/timerEnd.mp3");
       return;
     }
 
@@ -20,33 +41,43 @@ export default function SessionPage() {
     return () => {
       clearInterval(interval);
     };
-  }, [secondsLeft]);
+  }, [secondsLeft, router]);
 
   const formattedTime = formatTime(secondsLeft);
   const isTimerExpired = secondsLeft === 0;
 
   const continueClick = () => {
     setButtonAnimation("opacity-0 scale-125");
+    playSound("/continueClick.mp3");
     setTimeout(() => {
       setButtonsVisible(false);
-      setSecondsLeft(.1 * 60);
+      setSecondsLeft(15);
+      setTimerColor("text-blue-500");
       setButtonAnimation("");
     }, 500);
   };
 
   const breakClick = () => {
     setButtonAnimation("opacity-0 scale-125");
+    playSound("/breakClick.mp3");
     setTimeout(() => {
       setButtonsVisible(false);
-      setSecondsLeft(1);
+      setSecondsLeft(5);
+      setTimerColor("text-orange-400");
       setButtonAnimation("");
     }, 500);
   };
 
-  return (
-    <div className="bg-gradient-to-r from-sky-200 h-screen flex flex-col justify-center items-center">
+  const playSound = (src: string) => {
+    const audio = new Audio(src);
+    audio.play();
+  };
+
+  const page = !loading ? (
+    <div className="bg-gradient-to-r from-sky-200 h-screen flex flex-col justify-center items-center overflow-hidden">
+      <Navbar given_user={user} />
       <span
-        className={`text-blue-500 transition-all duration-500 ${
+        className={`${timerColor} h-full transition-all duration-500 ${
           isTimerExpired
             ? "opacity-0 transform scale-125"
             : "opacity-20 hover:opacity-40"
@@ -65,7 +96,7 @@ export default function SessionPage() {
         >
           <button
             onClick={continueClick}
-            className={`transform transition-transform hover:scale-110 ${buttonAnimation} duration-500 ease-in-out`}
+            className={`transform transition-all ${buttonAnimation} duration-500 ease-in-out hover:scale-110`}
           >
             <div className="bg-green-100 rounded-full p-9 shadow-lg text-6xl">
               👍
@@ -73,7 +104,7 @@ export default function SessionPage() {
           </button>
           <button
             onClick={breakClick}
-            className={`transform transition-transform hover:scale-110 ${buttonAnimation} duration-500 ease-in-out`}
+            className={`transform transition-all ${buttonAnimation} duration-500 ease-in-out hover:scale-110`}
           >
             <div className="bg-rose-200 rounded-full p-9 shadow-lg text-6xl">
               🖐️
@@ -82,6 +113,12 @@ export default function SessionPage() {
         </div>
       )}
     </div>
+  ) : (
+    <>Loading...</>
+  );
+
+  return (
+    <>{page}</>
   );
 }
 
