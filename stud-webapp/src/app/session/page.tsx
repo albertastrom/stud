6214b0defd8ca7caac9d6016723157c5aa8b003d
Hector Astrom <hector.astrom@gmail.com>
2023-10-22 02:53:53 -0400

    added animations & functionality to the timer and buttons

--- stud-webapp/src/app/session/page.tsx
+++ stud-webapp/src/app/session/page.tsx
@@ -3,10 +3,15 @@
 import { useState, useEffect } from "react";
 
 export default function SessionPage() {
-  const [secondsLeft, setSecondsLeft] = useState<number>(.5 * 60);
+  const [secondsLeft, setSecondsLeft] = useState<number>(.1 * 60);
+  const [buttonsVisible, setButtonsVisible] = useState(false);
+  const [buttonAnimation, setButtonAnimation] = useState("");
 
   useEffect(() => {
-    if (secondsLeft <= 0) return;
+    if (secondsLeft <= 0) {
+      setButtonsVisible(true);
+      return;
+    }
 
     const interval = setInterval(() => {
       setSecondsLeft((prevSeconds) => Math.max(prevSeconds - 1, 0));
@@ -20,24 +25,56 @@ export default function SessionPage() {
   const formattedTime = formatTime(secondsLeft);
   const isTimerExpired = secondsLeft === 0;
 
+  const continueClick = () => {
+    setButtonAnimation("opacity-0 scale-125");
+    setTimeout(() => {
+      setButtonsVisible(false);
+      setSecondsLeft(.1 * 60);
+      setButtonAnimation("");
+    }, 500);
+  };
+
+  const breakClick = () => {
+    setButtonAnimation("opacity-0 scale-125");
+    setTimeout(() => {
+      setButtonsVisible(false);
+      setSecondsLeft(1);
+      setButtonAnimation("");
+    }, 500);
+  };
+
   return (
-    <div className="bg-gradient-to-r from-sky-200 h-screen flex justify-center items-center">
+    <div className="bg-gradient-to-r from-sky-200 h-screen flex flex-col justify-center items-center">
       <span
-        className={`text-blue-500 ${
-          isTimerExpired ? "opacity-0" : "opacity-20"
-        } text-[300px] font-bold font-mono text-center`}
+        className={`text-blue-500 transition-all duration-500 ${
+          isTimerExpired
+            ? "opacity-0 transform scale-125"
+            : "opacity-20 hover:opacity-40"
+        } text-[300px] font-bold font-mono text-center mb-12`}
       >
         {formattedTime}
       </span>
 
-      {isTimerExpired && (
-        <div className="absolute w-full flex justify-between w-1/3">
-          <button className="transform transition-transform hover:scale-110">
+      {buttonsVisible && (
+        <div
+          className="flex justify-between w-1/3 transition-all transform duration-500 ease-in-out"
+          style={{
+            bottom: isTimerExpired ? "30%" : "0%",
+            position: "absolute",
+          }}
+        >
+          <button
+            onClick={continueClick}
+            className={`transform transition-transform hover:scale-110 ${buttonAnimation} duration-500 ease-in-out`}
+          >
             <div className="bg-green-100 rounded-full p-9 shadow-lg text-6xl">
               👍
             </div>
           </button>
-          <button className="transform transition-transform hover:scale-110">
+          <button
+            onClick={breakClick}
+            className={`transform transition-transform hover:scale-110 ${buttonAnimation} duration-500 ease-in-out`}
+          >
             <div className="bg-rose-200 rounded-full p-9 shadow-lg text-6xl">
               🖐️
             </div>
